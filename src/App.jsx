bdc815303ea80c578bac0f63d3a1362ec5fd87db
jemblonganvalley <jemblonganvalley@gmail.com>
2022-11-03 21:48:36 +0700

    gilang kampret

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Blog from './pages/Blog'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/blog' element={<Blog />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  )
+}
+
+export default App
